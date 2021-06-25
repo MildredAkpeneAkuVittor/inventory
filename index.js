e8b50c05c8f2db8f318dac83db14d9bffb912336
mildred <mildred.vittor@amalitech.org>
2021-06-25 16:08:03 +0000

    new

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 const contentTag = document.querySelector(".main");
 const infoWithOption=document.getElementById('alert-with-input');
 var code;
 var NameIt;
+infoArray=[]
+
 
 
 
@@ -25,41 +26,62 @@ var NameIt;
        
     }
    
-    function addNewField(input){  
+    function addNewField(input){  //add a new field called quantity, required for project
        let allProducts=input
       for(let i=0;i<allProducts.length;i++){
-        let quantity = Math.floor(Math.random() * 41);   
+        let quantity = Math.floor(Math.random() * 41); //generate random number for quantity  
         allProducts[i].quantity=quantity;
       }
       allProductsPlus=allProducts;
       lstorageReadWrite(false,"allProductsPlus",allProductsPlus)
+      location.reload();
+      
+
    }
  }
 
 
  if(!lstorageReadWrite(true,"allProductsPlus")){  
+     logger("APi fetch successful")
      fetchData();
  }
-
-
-
-function lstorageReadWrite(readOrWrite,name,content,token){    
+ function logger(msg){
+   
+    eventNew={}
+    var timeStamp = Number(new Date());
+    eventNew['timeStamp']= timeStamp;
+    eventNew["msg"]=msg;
     
+    if(!localStorage.getItem("events")){
+        infoArray.push(eventNew);
+        localStorage.setItem("events",(JSON.stringify(infoArray)));
+    }
+    else{
+    let events=JSON.parse(localStorage.getItem("events"));
+    events.push(eventNew);
+    localStorage.setItem("events",(JSON.stringify(events)));
+    }
+    displayAlert(1);
+    
+     
+}
+
+function lstorageReadWrite(readOrWrite,name,content,token){   
     if(readOrWrite===true){ 
         if(localStorage.getItem(name)){
-         let jsonString=localStorage.getItem(name);      
+         let jsonString=localStorage.getItem(name);       
          return JSON.parse(jsonString);
         }
         else{
-
-        }
+         console.log('No such object found')
          return 0;
+        }
     }
     else if(readOrWrite===false){  
         localStorage.setItem(name,JSON.stringify(content))
     }
     else if(readOrWrite==="delete"){
-             
+            
             if(alertHandler(true, "confirm delete")==="okay"){
                 localStorage.removeItem(content) 
             }        
@@ -67,7 +89,8 @@ function lstorageReadWrite(readOrWrite,name,content,token){
     else if(readOrWrite==="clear"){     
         
         
-        
+        console.log(code)
+        console.log(token)
         if (token===undefined){
             code = securityToken();
             console.log(code)
@@ -88,7 +111,7 @@ function lstorageReadWrite(readOrWrite,name,content,token){
 }
 
 
-function statisticsHandler(indicator,dataSet){ 
+function statisticsHandler(indicator,dataSet){ //choose the dataset for the calculation
     var workingSet;                     
     if (dataSet==="allProducts"){
         workingSet=lstorageReadWrite(true,'allProductsPlus');
@@ -104,11 +127,11 @@ function statisticsHandler(indicator,dataSet){
     if(indicator==="quantityByCategory"){
         var clothing_men=0;  var jewelery=0; var electronics=0; var clothing_women=0;
         for(let i=0;i<workingSet.length;i++){
-            if(workingSet.category==="men's clothing"){
+            if(workingSet[i].category==="men's clothing"){
                 clothing_men=clothing_men + 1
-            }else if(workingSet.category==="women's clothing"){
+            }else if(workingSet[i].category==="women's clothing"){
                 clothing_women = clothing_women + 1
-            }else if(workingSet.category==="electronics"){
+            }else if(workingSet[i].category==="electronics"){
                 electronics=electronics + 1
             }else{
                 jewelery = jewelery + 1
@@ -125,14 +148,14 @@ function statisticsHandler(indicator,dataSet){
     if(indicator==='stockValueByCategory'){
         var clothing_men=0;  var jewelery=0; var electronics=0; var clothing_women=0;
         for(let i=0;i<workingSet.length;i++){
-            if(workingSet.category==="men's clothing"){
-                clothing_men = clothing_men + (+workingSet.quantity * (+workingSet.price))
-            }else if(workingSet.category==="women's clothing"){
-                clothing_women = clothing_women + (+workingSet.quantity * (+workingSet.price))
-            }else if(workingSet.category==="electronics"){
-                electronics=electronics + (+workingSet.quantity * (+workingSet.price))
+            if(workingSet[i].category==="men's clothing"){
+                clothing_men = clothing_men + (+workingSet[i].quantity * (+workingSet[i].price))
+            }else if(workingSet[i].category==="women's clothing"){
+                clothing_women = clothing_women + (+workingSet[i].quantity * (+workingSet[i].price))
+            }else if(workingSet[i].category==="electronics"){
+                electronics=electronics + (+workingSet[i].quantity * (+workingSet[i].price))
             }else{
-                jewelery = jewelery + (+workingSet.quantity * (+workingSet.price))
+                jewelery = jewelery + (+workingSet[i].quantity * (+workingSet[i].price))
             }
         }
        
@@ -141,17 +164,20 @@ function statisticsHandler(indicator,dataSet){
                 "jewelery":jewelery,
                 "electronics": electronics,}    
     }
+    if(indicator==="totalItems"){
+       return workingSet.length
+    }
 
     if(indicator==="depletedStocks"){
         var depletedStocksArray=[];
-       
+        console.log(workingSet)
         for(let i=0;i<workingSet.length;i++){
             if(+workingSet[i].quantity<1){
                 
                 depletedStocksArray.push(workingSet[i].title)
             }
             }
-       
+            console.log(depletedStocksArray)
         if(depletedStocksArray.length>0){
             return depletedStocksArray;
         }else{
@@ -170,7 +196,7 @@ function sideBarSummary(){
     sideBarCategories=document.querySelector('.sum-categories')
     sideBarTotal.innerHTML=`Total Stock Value: $ ${statisticsHandler("totalStockValue","allProducts")}`
     depletedResponse=statisticsHandler("depletedStocks","allProducts")
-    
+    console.log(depletedResponse)
     depletedResponse.forEach((item,index)=>{
         let div=document.createElement('li');
         div.innerHTML=item.substring(0,18);
@@ -179,6 +205,19 @@ function sideBarSummary(){
     })
 }
 sideBarSummary();
+
+function topStatsHandler(){
+    let totalStockValue = statisticsHandler('totalStockValue',"allProducts")
+    let totalCategories = 4;
+    let totalItems = statisticsHandler('totalItems','allProducts')
+    let position = document.querySelectorAll('.main-res')
+    console.log(position)
+    position[0].innerHTML=totalCategories;
+    position[1].innerHTML=totalItems;
+    position[2].innerHTML=totalStockValue
+
+}
+topStatsHandler();
 
 function colorLabel(qty,i){
 
@@ -196,7 +235,7 @@ function colorLabel(qty,i){
 
 
 
-function tableFillHandler(){     
+function tableFiller(){     //fills the table with data from localstorage
      let allProductsPlus = lstorageReadWrite(true,"allProductsPlus");
      
      for(let i=0;i<allProductsPlus.length;i++){
@@ -209,35 +248,93 @@ function tableFillHandler(){
         rowList[5].innerHTML=allProductsPlus[i].price;
         rowList[6].innerHTML=allProductsPlus[i].quantity;
         rowList[7].className= colorLabel(allProductsPlus[i].quantity,i)
-        
+        console.log(allProductsPlus[i].quantity)
         
      }
 }
-tableFillHandler()
+tableFiller()
 
 
 function htmlGenerator(i){  
 
     newRow = document.createElement("tr");
-    newRow.className = 'rows row-'+ i;
+    newRow.className = ' row-'+ i;
     document.querySelector('.row-template').appendChild(newRow);
 
-    createRowContent = `<td><input type="checkbox" class="check item${i}"></td>
-                        <td><div class="label-code item${i}"></div></td>
-                        <td><div class="category item${i}"></div></td>
-                        <td><div class="name item${i}"></div></td>
-                        <td><div class="description item${i}"></div></td>
-                        <td><div class="label-code item${i}">price</div></td>
-                        <td><div class="quantity item${i}"></div></td>
-                        <td><div class=" item${i}"><div class="label"></div></div></td> 
-                        <td><div class="edit item${i}"><a href="./details.html"><button class="edit">Edit</button></a></div></td>`
-    document.querySelector(`.row-${i}`).innerHTML = createRowContent;
+    createRowContent = `<td class="rows"><input type="checkbox" class="check item${i}"></td>
+                        <td class="rows"><div class="label-code item${i}"></div></td>
+                        <td class="rows"><div class="category item${i}"></div></td>
+                        <td class="rows"><div class="name item${i}"></div></td>
+                        <td class="rows"><div class="description item${i}"></div></td>
+                        <td class="rows"><div class="label-code item${i}">price</div></td>
+                        <td class="rows"><div class="quantity item${i}"></div></td>
+                        <td class="rows"><div class=" item${i}"><div class="label"></div></div></td> 
+                        <td><div><button class="edit" id="edit${i}">Edit</button></div></td>`
+document.querySelector(`.row-${i}`).innerHTML = createRowContent;
 
 }
 
+//Interactive popup message handler for entire web page below!!!!
+function alertHandler(reqres,message,from,token){ //reqres is boolean, message is string, true means response required
+    
 
+    if (typeof(reqres) !== "boolean" || message===""){ //reqres is only true when response is required.
+             console.log('alerthandler error! request-response not clear, or message is empty!')
+         }
+    else{
+        if(reqres===true){      
+           return callAlert(1,message,token);
+        }
+        else{
+            callAlert(0,message);
+        }
+    }
 
-function securityToken(){  
+    function callAlert(type,message,token){  //typeof(type) = number, 0 for show-info, 1 for alert with input
+     
+        let justAlert = `<div id="show-info">
+                            <p id="info-p">${message}</p>
+                            <button id="okay-and-nothing">Okay</button>
+                        </div>`
+        let notJustAlert=`<div id="alert-with-input">
+                            <p id="info-input-p">${message}</p>
+                            <button id="cancel" name="cancel" class="do-this">Cancel</button>
+                            <button id="okay" name = "okay"class="do-this">Okay</button>
+                          </div>`        
+        if (type === 0 ){                                          //
+            document.querySelector('.back-drop').style.display="block";//className = ('back-drop visibility');
+           
+            contentTag.insertAdjacentHTML('afterbegin',justAlert)
+            document.getElementById("okay-and-nothing").addEventListener('click',(e)=>{
+                document.getElementById('show-info').remove();
+                document.querySelector('.back-drop').style.display="none"//className = ('back-drop');
+            });
+        } 
+        else{
+            var code = token;
+            var sender = from;
+            document.querySelector('.back-drop').className = ('back-drop visibility');
+           
+            contentTag.insertAdjacentHTML('afterbegin',notJustAlert);
+            document.querySelectorAll('.do-this').forEach(element => { 
+                element.addEventListener('click', (e)=>{
+                    var result = e.target.getAttribute('name');
+                    console.log(code)
+                    window[sender]("clear",undefined,result,token);
+                 
+                   document.getElementById('alert-with-input').remove();
+                   document.querySelector('.back-drop').className = ('back-drop')
+                   
+                })
+                
+            })
+                    
+        }  
+    }     
+
+}
+
+function securityToken(){   
 
     let CodeSaverArray = [];
     let hexRef = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','0'];
@@ -258,6 +355,7 @@ function buildSearchArray(){
         categoriesArray[i] = allProducts[i].category;
     }
 }
+
 function search(){
     
 }
@@ -267,13 +365,13 @@ quickEditButtons.forEach(element => {
     element.addEventListener('click',(e)=>{
         thisEvent=e.target.getAttribute("id").match(/\d+/g,'')
         quickEditHandler(thisEvent)
-        console.log(thisEvent)
+        
     })
 });
 
 function quickEditHandler(eventString){
     localStorage.setItem("forEditPage",JSON.stringify(eventString))
-    location.assign("./edit.html")
+    location.assign("./resources/edit.html")
 }
 
 
@@ -288,23 +386,41 @@ rows.forEach(element => {
 });
 function itemEventsHandler(eventString){
   localStorage.setItem("forDetailsPage",JSON.stringify(eventString))
-  location.assign("./details.html")
+  location.assign("resources/details.html")
 }
 
 
 
 
-
-google.charts.load('current', {'packages':['corechart']});
-
-
-google.charts.setOnLoadCallback(drawChart);
-
-
-
-
-  
  
 
+let resOpen = document.querySelector('.res-open')
+let resClose = document.querySelector('.res-close')
+
+resOpen.addEventListener("click",(e)=>{
+    document.querySelector('.side-responsive').style.display="block"
+    document.querySelector('.side-menu').style.display="none"
+})
+
+resClose.addEventListener('click',(e)=>{ 
+        document.querySelector('.side-responsive').style.display='none'
+        document.querySelector('.side-menu').style.display='block'
+})
 
 
+function displayAlert(count){
+    let counter;
+    if (count !==1){counter=0;}
+ 
+ if(!JSON.parse(localStorage.getItem('alertCounter'))){
+ 
+  document.querySelector('.bell-back').innerHTML=counter;
+  localStorage.setItem('alertCounter',JSON.stringify(counter))
+ }else{
+     alertCount= +JSON.parse(localStorage.getItem('alertCounter'))+counter;
+     document.querySelector('.bell-back').innerHTML=alertCount;
+     localStorage.setItem('alertCounter',JSON.stringify(alertCount))
+ }
+
+}
+displayAlert()
